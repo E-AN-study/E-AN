5336e90399d6119f590e3ce88758dcb0434d34e9
yunsusu <rladbstn1212@gmail.com>
2024-01-05 13:42:48 +0900

    fix : router 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import ChapterListPage from "../src/pages/ChapterListPage";
+import Edit from "./pages/edit";
+import MainPage from "./pages/main";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/list" element={<ChapterListPage />} />
+          <Route path="/edit" element={<Edit />} />
+          {/* <Route path="/list" element={<ChapterListPage />} /> */}
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/pages/main/index.jsx
@@ -0,0 +1,18 @@
+// import React from "react";
+// import { Helmet } from "react-helmet";
+import MainLogo from "./MainLogo";
+import SortButton from "./SortButton";
+
+function MainPage() {
+  return (
+    <>
+      {/* <Helmet> */}
+      <title>E.AN</title>
+      {/* </Helmet> */}
+      <MainLogo />
+      <SortButton />
+    </>
+  );
+}
+
+export default MainPage;
