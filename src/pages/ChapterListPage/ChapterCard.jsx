4d7beba86a52126341200d66364688890746ad50
yunsusu <rladbstn1212@gmail.com>
2024-01-05 14:11:41 +0900

    fix : propTypes 수정

--- src/pages/ChapterListPage/ChapterCard.jsx
+++ src/pages/ChapterListPage/ChapterCard.jsx
@@ -1,4 +1,10 @@
 import "./ChapterCard.scss";
+import PropTypes from "prop-types";
+
+const propTypes = {
+  title: PropTypes.string.isRequired,
+  index: PropTypes.number.isRequired,
+};
 
 function ChapterCard({ title, index }) {
   return (
@@ -6,11 +12,13 @@ function ChapterCard({ title, index }) {
       <div className="chapter-card-badge">Chapter {index}</div>
       <div className="chapter-card-title">{title}</div>
       <div className="chapter-card-receivedquestion-section">
-        <img src="../../src/assets/Messages.svg" />
+        <img src="../../src/assets/Messages.svg" alt="Messages" />
         <div className="chapter-card-receivedquestion">작성된 글</div>
       </div>
     </button>
   );
 }
+
+ChapterCard.propTypes = propTypes;
 
 export default ChapterCard;
