2beaf11780ebf779ca3f299bbc37e161d33aaea4
JunCastle <juncastle97@gmail.com>
2024-01-08 14:28:37 +0900

    Feat: 댓글 눌렀을 때 댓글 창 띄우기 & 버튼 클릭 시 페이지 이동

--- src/pages/TextList/TextList.jsx
+++ src/pages/TextList/TextList.jsx
@@ -1,5 +1,6 @@
 import styles from "./TextList.module.scss";
 import classNames from "classnames/bind";
+import { Link } from "react-router-dom";
 import logo from "../../assets/EAN-logo.png";
 import linkIcon from "../../assets/link.svg";
 import kakaotalk from "../../assets/kakaotalk.svg";
@@ -12,7 +13,9 @@ const cx = classNames.bind(styles);
 export function TextList() {
   return (
     <div className={cx("container")}>
-      <img className={cx("logoImg")} src={logo} alt="logo image" />
+      <Link to="/">
+        <img className={cx("logoImg")} src={logo} alt="logo image" />
+      </Link>
       <h1 className={cx("title")}>챕터 1 CPU</h1>
       <div className={cx("shareButton")}>
         <button className={cx("linkIcon")}>
@@ -23,9 +26,11 @@ export function TextList() {
         </button>
       </div>
       <div className={cx("textCardList")}>
-      <div className={cx("listBtn")}>
-        <Button text={"글 목록"} />
-      </div>
+        <div className={cx("listBtn")}>
+          <Link to="/List">
+            <Button text={"글 목록"} />
+          </Link>
+        </div>
         <div className={cx("listTitle")}>
           <img className={cx("messageIcon")} src={messages} alt="logo image" />
           이미 n명이 공부했습니다.
@@ -39,7 +44,9 @@ export function TextList() {
         <TextCard></TextCard>
       </div>
       <div className={cx("studyBtn")}>
-        <Button text={"나도 공부하기"} />
+        <Link to="/Edit">
+          <Button text={"나도 공부하기"} />
+        </Link>
       </div>
     </div>
   );
