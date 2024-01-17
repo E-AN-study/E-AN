b0f3e2235a1e60f3c5728a8974bd6561a65e166d
parkjiwon <bjiwon0324@gmail.com>
2024-01-17 17:05:18 +0900

    fix: url 정규식 제거

--- src/pages/edit/index.jsx
+++ src/pages/edit/index.jsx
@@ -1,15 +1,14 @@
-import styles from "./edit.module.scss";
-import classNames from "classnames/bind";
-import editIcon from "../../assets/Messages.svg";
-import { Link, Navigate, useParams, useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
-import profile1 from "../../assets/profile1.jpg";
-import profile2 from "../../assets/profile2.jpg";
-import profile3 from "../../assets/sample.png";
+import styles from './edit.module.scss';
+import classNames from 'classnames/bind';
+import editIcon from '../../assets/Messages.svg';
+import { Link, useParams, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import profile1 from '../../assets/profile1.jpg';
+import profile2 from '../../assets/profile2.jpg';
+import profile3 from '../../assets/sample.png';
+import Question from './Question';
 
-import Question from "./Question";
-
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_APP_KEY;
 const supabaseKey = import.meta.env.VITE_APP_SECRET_CODE;
@@ -17,20 +16,19 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 
 const cx = classNames.bind(styles);
 
-const urlRegex = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?$/;
-
 export default function Edit() {
-  const [question, setQuestion] = useState("");
+  const [question, setQuestion] = useState('');
   const [postData, setPostData] = useState({});
-  const [isValidUrl, setIsValidUrl] = useState(false);
-  const [profileImg, setProfileImg] = useState("");
+  const [profileImg, setProfileImg] = useState('');
 
   const { id } = useParams();
   const navigate = useNavigate();
 
   async function addUser(name, link, qs, profile, comment) {
-    let { data, error } = await supabase.from(`ean${id}`).insert([{ name, link, qs, profile, comment }]);
-    if (error) console.log("Error", error);
+    let { data, error } = await supabase
+      .from(`ean${id}`)
+      .insert([{ name, link, qs, profile, comment }]);
+    if (error) console.log('Error', error);
     else return data;
   }
 
@@ -44,50 +42,44 @@ export default function Edit() {
     const dataKey = e.target.id.slice(4).toLowerCase();
     const dataValue = e.target.value;
 
-    if (postData.url !== "" && dataKey === "url") {
-      setIsValidUrl(urlRegex.test(dataValue));
-
-      isValidUrl ? e.target.classList.remove(cx("error")) : e.target.classList.add(cx("error"));
-    }
-
     setPostData((prev) => ({ ...prev, [dataKey]: dataValue }));
   };
 
   return (
-    <div className={cx("wrap")}>
-      <div className={cx("editInner")}>
-        <div className={cx("editTitle")}>
+    <div className={cx('wrap')}>
+      <div className={cx('editInner')}>
+        <div className={cx('editTitle')}>
           <h1>
-            <img src={editIcon} className={cx("editIcon")} alt="edit아이콘" />
+            <img src={editIcon} className={cx('editIcon')} alt="edit아이콘" />
             공부를 하세요
           </h1>
           <Link to={`/textList/${id}`}>
             <p>X</p>
           </Link>
         </div>
-        <form onSubmit={submit} className={cx("editForm")}>
+        <form onSubmit={submit} className={cx('editForm')}>
           <div>
             <label htmlFor="editName">내 이름</label>
             <input
               type="text"
               id="editName"
-              className={cx("editInput", "editName")}
+              className={cx('editInput', 'editName')}
               placeholder="이름을 입력해주세요"
               onChange={handleChange}
             />
           </div>
-          <div className={cx("profileImg")}>
+          <div className={cx('profileImg')}>
             <label>내 얼굴</label>
             <input
               name="profileImg"
               type="radio"
               id="profile1"
-              className={cx("editInput", "profile")}
+              className={cx('editInput', 'profile')}
               onClick={() => {
-                setProfileImg("profile1");
+                setProfileImg('profile1');
               }}
             />
-            <label htmlFor="profile1" className={cx("profileLabel")}>
+            <label htmlFor="profile1" className={cx('profileLabel')}>
               <img src={profile1} />
             </label>
 
@@ -95,12 +87,12 @@ export default function Edit() {
               name="profileImg"
               type="radio"
               id="profile2"
-              className={cx("editInput", "profile")}
+              className={cx('editInput', 'profile')}
               onClick={() => {
-                setProfileImg("profile2");
+                setProfileImg('profile2');
               }}
             />
-            <label htmlFor="profile2" className={cx("profileLabel")}>
+            <label htmlFor="profile2" className={cx('profileLabel')}>
               <img src={profile2} />
             </label>
 
@@ -108,12 +100,12 @@ export default function Edit() {
               name="profileImg"
               type="radio"
               id="profile3"
-              className={cx("editInput", "profile")}
+              className={cx('editInput', 'profile')}
               onClick={() => {
-                setProfileImg("profile3");
+                setProfileImg('profile3');
               }}
             />
-            <label htmlFor="profile3" className={cx("profileLabel")}>
+            <label htmlFor="profile3" className={cx('profileLabel')}>
               <img src={profile3} />
             </label>
           </div>
@@ -122,23 +114,22 @@ export default function Edit() {
             <input
               type="text"
               id="editUrl"
-              className={cx("editInput", "editUrl")}
+              className={cx('editInput', 'editUrl')}
               placeholder="링크를 입력해주세요"
               onChange={handleChange}
             />
-            {postData.url && !isValidUrl && <p className={cx("errorMsg")}>올바른 주소를 입력해 주세요</p>}
           </div>
           <div>
             <label htmlFor="editQuestion">이거 모르겠어...</label>
             <Question
               onChange={setQuestion}
               id="editQuestion"
-              className={cx("editInput", "editQuestion")}
+              className={cx('editInput', 'editQuestion')}
               placeholder="질문을 입력해주세요"
             ></Question>
           </div>
 
-          <button type="submit" className={cx("editButton")} onClick={submit}>
+          <button type="submit" className={cx('editButton')} onClick={submit}>
             글 작성하기
           </button>
         </form>
